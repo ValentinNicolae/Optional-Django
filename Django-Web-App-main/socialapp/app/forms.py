81a724f4651fe43cea74b29c8ab8e420d9095e97
Nicolae Valentin <46230035+ValentinNicolae@users.noreply.github.com>
2021-01-13 12:55:36 +0200

    Add files via upload

---
+++ Django-Web-App-main/socialapp/app/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from app import models
+
+
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = models.Comment
+#         fields = ['text']
+
+
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = models.Post
+#         fields = ['text']
+
+# class UserProfileForm(forms.ModelForm):
+#     first_name = forms.CharField(max_length=100)
+#     last_name = forms.CharField(max_length=100)
+#     e_mail = forms.CharField(max_length=100)
+#     class Meta:
+#         model =models.UserProfile
+#         exclude = ['user', 'friend_requests', 'friends']
